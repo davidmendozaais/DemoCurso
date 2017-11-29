f06af9237b689ecf0b31fa81c499dd99754c451b
AIS\davidm <david.mendoza@ais-int.com>
2017-11-29 13:49:52 +0100

    Añadido comentario de prueba

--- src/main/java/com/ais/thread/MyRunnable.java
+++ src/main/java/com/ais/thread/MyRunnable.java
@@ -4,6 +4,7 @@ public class MyRunnable implements Runnable{
 
 	public void run() {
 		// TODO Auto-generated method stub
+		// Comentario de prueba
 		System.out.println("Thread name: " + Thread.currentThread().getName());
 	}
 
